1733d73468791f7cab0fc0b5370279d2ad262cf7
KatherineMcConnell <mcconnell.katherine01@gmail.com>
2021-03-10 10:25:22 -0700

    Assert;pass test_it_exists

--- test/market_test.rb
+++ test/market_test.rb
@@ -6,4 +6,10 @@ require './lib/vendor'
 require 'pry'
 
 class MarketTest < Minitest::Test
+
+  def test_it_exists
+    market = Market.new("South Pearl Street Farmers Market")
+
+    assert_instance_of Market, market
+  end
 end
